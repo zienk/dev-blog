e3314d1806ba694ab867aa3e0632d176b14d47de
zienk <zienkdev@gmail.com>
2025-08-26 19:49:13 +0700

    feat(core): Add IRepository and IUnitOfWork interfaces

---
+++ src/DevBlog.Core/SeedWorks/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq.Expressions;
+
+namespace DevBlog.Core.SeedWorks
+{
+    public interface IRepository <TEntity, TKey> where TEntity : class
+    {
+        Task<TEntity> GetByIdAsync(TKey id);
+        Task<IEnumerable<TEntity>> GetAllAsync();
+        IEnumerable<TEntity> Find(Expression<Func<TEntity, bool>> expression);
+        void Add(TEntity entity);
+        void AddRange(IEnumerable<TEntity> entities);
+        void Remove(TEntity entity);
+        void RemoveRange(IEnumerable<TEntity> entities);
+    }
+}
---
+++ src/DevBlog.Core/SeedWorks/IUnitOfWork.cs
@@ -0,0 +1,7 @@
+﻿namespace DevBlog.Core.SeedWorks
+{
+    public interface IUnitOfWork
+    {
+        Task<int> CompleteAsync();
+    }
+}
