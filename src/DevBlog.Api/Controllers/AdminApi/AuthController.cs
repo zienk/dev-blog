ac33c10d2a0b8cbfa1ed43dbfc0613dd4a691c1d
zienk <zienkdev@gmail.com>
2025-08-29 18:45:12 +0700

    feat(authorized): Add role-based permission claims to AuthController
    Implemented logic in AuthController to include user permissions as claims during authentication, based on user roles. Added ClaimExtensions for permission handling, RoleClaimsDto model, and a Roles constants class. Refactored Permissions.cs to remove unused usings.

--- src/DevBlog.Api/Controllers/AdminApi/AuthController.cs
+++ src/DevBlog.Api/Controllers/AdminApi/AuthController.cs
@@ -1,11 +1,16 @@
-﻿using DevBlog.Api.Services;
+﻿using AutoMapper.Internal;
+using DevBlog.Api.Extensions;
+using DevBlog.Api.Services;
 using DevBlog.Core.Entities.Identity;
 using DevBlog.Core.Models.Auth;
+using DevBlog.Core.Models.System;
 using DevBlog.Core.SeedWorks.Constants;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System.IdentityModel.Tokens.Jwt;
+using System.Reflection;
 using System.Security.Claims;
+using System.Text.Json;
 
 namespace DevBlog.Api.Controllers.AdminApi
 {
@@ -16,17 +21,22 @@ namespace DevBlog.Api.Controllers.AdminApi
         private readonly UserManager<AppUser> _userManager;
         private readonly SignInManager<AppUser> _signInManager;
         private readonly ITokenService _tokenService;
+        private readonly RoleManager<AppRole> _roleManager;
 
-        public AuthController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager, ITokenService tokenService)
+        public AuthController(UserManager<AppUser> userManager,
+                              SignInManager<AppUser> signInManager,
+                              ITokenService tokenService,
+                              RoleManager<AppRole> roleManager)
         {
             _userManager = userManager;
             _signInManager = signInManager;
             _tokenService = tokenService;
+            _roleManager = roleManager;
         }
 
         [HttpPost]
         public async Task<ActionResult<AuthenticatedResult>> Login([FromBody] LoginRequest request)
-        {   
+        {
             // Authentication
             if (request == null)
             {
@@ -47,6 +57,7 @@ namespace DevBlog.Api.Controllers.AdminApi
 
             // Authorization
             var role = await _userManager.GetRolesAsync(user);
+            var permissions = await this.GetPermisionsByUserIdAsync(user.Id.ToString());
             var claims = new[]
             {
                 new Claim(JwtRegisteredClaimNames.Email, user.Email),
@@ -55,7 +66,7 @@ namespace DevBlog.Api.Controllers.AdminApi
                 new Claim(ClaimTypes.Name, user.UserName),
                 new Claim(UserClaims.FirstName, user.FirstName),
                 new Claim(UserClaims.Roles, string.Join(",", role)),
-                //new Claim(UserClaims.Permissions, JsonSerialize.Serialize(permissions)),
+                new Claim(UserClaims.Permissions, JsonSerializer.Serialize(permissions)),
                 new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
             };
 
@@ -72,6 +83,37 @@ namespace DevBlog.Api.Controllers.AdminApi
                 Token = accessToken,
                 RefreshToken = refreshToken
             });
+        }
+
+        private async Task<List<string>> GetPermisionsByUserIdAsync(string userId)
+        {
+            var user = await _userManager.FindByIdAsync(userId);
+            var roles = await _userManager.GetRolesAsync(user);
+            var permissions = new List<string>();
+
+            var allPermissions = new List<RoleClaimsDto>();
+
+            if (roles.Contains(Roles.Admin))
+            {
+                var types = typeof(Permissions).GetTypeInfo().DeclaredNestedTypes;
+                foreach (var type in types)
+                {
+                    allPermissions.GetPermission(type);
+                }
+                permissions.AddRange(allPermissions.Select(p => p.Value));
+            }
+            else
+            {
+                foreach (var roleName in roles)
+                {
+                    var role = await _roleManager.FindByNameAsync(roleName);
+                    var claims = await _roleManager.GetClaimsAsync(role);
+                    var roleClaimValues = claims.Select(c => c.Value).ToList();
+                    permissions.AddRange(roleClaimValues);
+                }
+            }
+
+            return permissions.Distinct().ToList();
         }
     }
 }
---
+++ src/DevBlog.Api/Extensions/ClaimExtensions.cs
@@ -0,0 +1,39 @@
+﻿using DevBlog.Core.Entities.Identity;
+using DevBlog.Core.Models.System;
+using Microsoft.AspNetCore.Identity;
+using System.ComponentModel;
+using System.Reflection;
+using System.Security.Claims;
+
+namespace DevBlog.Api.Extensions
+{
+    public static class ClaimExtensions
+    {
+        public static void GetPermission(this List<RoleClaimsDto> allPermissions, Type policy)
+        {
+            FieldInfo[] fields = policy.GetFields(BindingFlags.Public | BindingFlags.Static);
+
+            foreach (FieldInfo field in fields)
+            {
+                var attribute = field.GetCustomAttributes(typeof(DescriptionAttribute), true);
+                string displayName = field.GetValue(null).ToString();
+                var attributes = field.GetCustomAttributes(typeof(DescriptionAttribute), true);
+                if (attributes.Length > 0)
+                {
+                    var description = (DescriptionAttribute)attributes[0];
+                    displayName = description.Description;
+                }
+                allPermissions.Add(new RoleClaimsDto { Type = "Permission", Value = field.GetValue(null).ToString(), DisplayName = displayName});
+            }
+        }
+
+        public static async Task AddPermissionClaim(this RoleManager<AppRole> roleManager, AppRole role, string permission)
+        {
+            var claims = await roleManager.GetClaimsAsync(role);
+            if (!claims.Any(c => c.Type == "Permission" && c.Value == permission))
+            {
+                await roleManager.AddClaimAsync(role, new Claim("Permission", permission));
+            }
+        }
+    }
+}
