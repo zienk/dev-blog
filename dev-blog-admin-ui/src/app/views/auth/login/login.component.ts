8b885e9993b953411ec7e54afef4866e1449def2
zienk <zienkdev@gmail.com>
2025-08-31 19:27:54 +0700

    feat(login): Implement reactive login form with authentication
    Replaces the static login form with a reactive Angular form using FormBuilder and form controls. Integrates authentication via AdminApiAuthApiClient, handles login submission, error display, and redirects to the dashboard on success.

--- dev-blog-admin-ui/src/app/views/auth/login/login.component.ts
+++ dev-blog-admin-ui/src/app/views/auth/login/login.component.ts
@@ -14,10 +14,52 @@ import {
   InputGroupTextDirective,
   RowComponent
 } from '@coreui/angular';
+import { FormBuilder, FormControl, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+
+import { AdminApiAuthApiClient, AuthenticatedResult, LoginRequest } from 'src/app/api/admin-api.service.generated';
+
+import { AlertService } from 'src/app/shared/services/alert.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  imports: [ContainerComponent, RowComponent, ColComponent, CardGroupComponent, CardComponent, CardBodyComponent, FormDirective, InputGroupComponent, InputGroupTextDirective, IconDirective, FormControlDirective, ButtonDirective, NgStyle]
+  imports: [ContainerComponent, RowComponent, ColComponent, CardGroupComponent, CardComponent, CardBodyComponent, FormDirective, InputGroupComponent, InputGroupTextDirective, IconDirective, FormControlDirective, ButtonDirective, NgStyle, ReactiveFormsModule]
 })
-export class LoginComponent {}
+export class LoginComponent {
+  loginForm: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    private authApiClient: AdminApiAuthApiClient,
+    private alertService: AlertService,
+    private router: Router
+  ) {
+    this.loginForm = this.fb.group({
+      userName: new FormControl('', Validators.required),
+      password: new FormControl('', Validators.required)
+    })
+  }
+
+  login() {
+    var request: LoginRequest = new LoginRequest({
+      username: this.loginForm.controls['userName'].value,
+      password: this.loginForm.get('password')?.value
+    });
+
+    this.authApiClient.login(request).subscribe({
+      next: (res: AuthenticatedResult) => {
+        //Save token and refresh token to localstorage
+
+        //Redirect to dashboard
+        this.router.navigate(['/dashboard']);
+      },
+      error: (error: any) => {
+        // Handle error
+        console.error('Login error:', error);
+        this.alertService.showError('Login failed. Please try again.');
+      },
+
+    });
+  }
+}
