2932b318637f806f2c43655134d4eb1942b329d1
zienk <zienkdev@gmail.com>
2025-08-31 19:26:19 +0700

    chore(frontend): Add PrimeNG and PrimeIcons dependencies
    Integrated PrimeNG and PrimeIcons by updating package.json and angular.json, adding required styles, and importing FormsModule and ReactiveFormsModule in AuthModule. This prepares the project for using PrimeNG UI components.

--- dev-blog-admin-ui/src/app/views/auth/auth.module.ts
+++ dev-blog-admin-ui/src/app/views/auth/auth.module.ts
@@ -8,13 +8,11 @@ import { Page404Component } from './page404/page404.component';
 import { Page500Component } from './page500/page500.component';
 import { ButtonModule, CardModule, FormModule, GridModule } from '@coreui/angular';
 import { IconModule } from '@coreui/icons-angular';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [
-    
-
-
 
   ],
   imports: [
@@ -24,7 +22,9 @@ import { IconModule } from '@coreui/icons-angular';
     ButtonModule,
     GridModule,
     IconModule,
-    FormModule
+    FormModule,
+    FormsModule,
+    ReactiveFormsModule
   ]
 })
 export class AuthModule {
