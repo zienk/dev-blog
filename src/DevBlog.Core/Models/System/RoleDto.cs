b0acabb230f19b217c3456be9df79aeb7d62828d
zienk <zienkdev@gmail.com>
2025-09-08 09:13:21 +0700

    feat(api): Add role management API and related models
    Introduces RoleController for admin role CRUD operations, including endpoints for creating, updating, deleting, and retrieving roles. Adds CreateUpdateRoleRequest and RoleDto models to support role data transfer and mapping.

---
+++ src/DevBlog.Core/Models/System/RoleDto.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using DevBlog.Core.Entities.Identity;
+
+namespace DevBlog.Core.Models.System
+{
+    public class RoleDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } 
+        public string DisplayName { get; set; }
+
+        public class AutoMapperProfile : Profile
+        {
+            public AutoMapperProfile()
+            {
+                CreateMap<AppRole, RoleDto>();
+            }
+        }
+
+    }
+}
